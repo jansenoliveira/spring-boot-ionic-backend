f242296ba326cd22f915b4512ca055b451d9c555
Jansen Oliveira <jansenoliveira94@gmail.com>
2019-08-27 14:18:56 -0300

    Endpoint/pedidos/{id} disponibilizado

--- src/main/java/com/jansen/cursomc/resources/PedidoResource.java
+++ src/main/java/com/jansen/cursomc/resources/PedidoResource.java
@@ -7,21 +7,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.jansen.cursomc.domain.Cliente;
-import com.jansen.cursomc.services.ClienteService;
+import com.jansen.cursomc.domain.Pedido;
+import com.jansen.cursomc.services.PedidoService;
 
 @RestController
-@RequestMapping(value = "/clientes")
-public class ClienteResource {
+@RequestMapping(value = "/pedidos")
+public class PedidoResource {
 
 	@Autowired
-	private ClienteService service;
+	private PedidoService service;
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		
-		Cliente obj = service.buscar(id);
-		
+
+		Pedido obj = service.buscar(id);
+
 		return ResponseEntity.ok(obj);
 	}
 
