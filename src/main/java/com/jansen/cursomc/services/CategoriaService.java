1fd7afdd21c0cb169b5266e35ba2a9a36d9051f8
Jansen Oliveira <jansenoliveira94@gmail.com>
2019-08-28 14:37:08 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/jansen/cursomc/services/CategoriaService.java
+++ src/main/java/com/jansen/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.jansen.cursomc.domain.Categoria;
@@ -18,22 +21,26 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
+	//Busca Categoria pelo ID
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
+	//Inseri uma nova Categoria
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
 	
+	//Atualiza Categoria a partir do ID
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
 	
+	//Deleta Categoria a partir do ID
 	public void delete(Integer id) {
 		find(id);
 		
@@ -45,8 +52,15 @@ public class CategoriaService {
 		} 
 	}
 	
+	//Lista todas as Categorias
 	public List<Categoria> findAll(){
 		return repo.findAll();
+	}
+	
+	//Serviço de paginação
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 
 }
