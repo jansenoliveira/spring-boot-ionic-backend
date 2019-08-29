123ddf9b38b29a5fa4dd1a38c2a857a5a63afa66
Jansen Oliveira <jansenoliveira94@gmail.com>
2019-08-29 17:11:33 -0300

    Correções: update em CategoriasService e construtor de Pagamento

--- src/main/java/com/jansen/cursomc/services/CategoriaService.java
+++ src/main/java/com/jansen/cursomc/services/CategoriaService.java
@@ -37,8 +37,9 @@ public class CategoriaService {
 	
 	//Atualiza Categoria a partir do ID
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj,obj);
+		return repo.save(newObj);
 	}
 	
 	//Deleta Categoria a partir do ID
@@ -66,6 +67,11 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDTO) {
 		return new Categoria(objDTO.getId(), objDTO.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+	
 	}
 
 }
