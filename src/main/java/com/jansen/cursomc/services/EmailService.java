720b75d9d205bcefcdbe5b5e83591beffbaa36f2
Jansen Oliveira <jansenoliveira94@gmail.com>
2019-09-09 15:31:02 -0300

    Implementando toString de Pedido

---
+++ src/main/java/com/jansen/cursomc/services/EmailService.java
@@ -0,0 +1,16 @@
+package com.jansen.cursomc.services;
+
+
+import org.springframework.mail.SimpleMailMessage;
+
+import com.jansen.cursomc.domain.Cliente;
+import com.jansen.cursomc.domain.Pedido;
+
+public interface EmailService {
+
+	void sendOrderConfirmationEmail(Pedido obj);
+	
+	void sendEmail(SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String msg);
+}
